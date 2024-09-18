018e63307023fba72c6e6c9f5ef4d69337f3b93b
SKT-Sukatat <82657530+SKT-Sukatat@users.noreply.github.com>
2024-09-18 13:28:37 +0700

    SQL50_08_09

---
+++ SQL50/08_Customer_Not_Buy.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+
+SELECT
+    customer_id,
+    COUNT(customer_id) count_no_trans
+FROM Visits v
+LEFT JOIN Transactions t
+ON v.visit_id = t.visit_id
+WHERE transaction_id IS NULL
+GROUP BY customer_id;
