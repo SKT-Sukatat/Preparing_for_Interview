c85a28486347c5bef48327102c6959355768f3b8
SKT-Sukatat <82657530+SKT-Sukatat@users.noreply.github.com>
2024-09-18 12:32:48 +0700

    Add SQL50_06_07

---
+++ SQL50/06_Employee_ID_uni.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+
+SELECT
+    uni.unique_id,
+    e.name
+FROM Employees e
+LEFT JOIN EmployeeUNI uni
+ON e.id = uni.id;
---
+++ SQL50/07_Product_Analysis_01.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+
+SELECT
+    product_name,
+    year,
+    price
+FROM Sales
+LEFT JOIN Product
+USING(product_id);
