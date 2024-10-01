3df335792bc4e39cba3376820e3c0d137cb94cfb
SKT-Sukatat <82657530+SKT-Sukatat@users.noreply.github.com>
2024-10-01 15:49:41 +0700

    Add_SQL50_35

--- SQL50/35_product_price_at_a_given_date.sql
+++ SQL50/35_product_price_at_a_given_date.sql
@@ -1,6 +1,12 @@
 # Write your MySQL query statement below
 
 SELECT
+    product_id,
+    new_price as price
+FROM Products
+WHERE (product_id,change_date) IN (SELECT product_id , max(change_date) AS date FROM Products WHERE change_date <='2019-08-16' group by product_id)
+UNION
+SELECT
     DISTINCT product_id, 10 AS price
 FROM Products
-WHERE product_id NOT IN (SELECT DISTINCT product_id FROM Products WHERE change_date <='2019-08-16' )
+WHERE product_id NOT IN (SELECT DISTINCT product_id FROM Products WHERE change_date <='2019-08-16')
